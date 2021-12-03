6aea7b62cfb067ae45e600719c1a4f2165ffff20
David Jones <david@omg.lol>
2021-12-03 17:16:11 +0000

    hacking

--- 2021/day/3/mod.ts
+++ 2021/day/3/mod.ts
@@ -1,39 +1,37 @@
-export function parseDiagnostics(input: string): number[] {
-  return input.match(/[01]+/g)!.map((bits) => Number.parseInt(bits, 2));
+type Bit = 0 | 1;
+
+export function parseDiagnostics(input: string): Bit[][] {
+  return input.match(/[01]+/g)!.map((row) =>
+    Array.from(row, (c) => c === "1" ? 1 : 0)
+  );
 }
 
-export function* getBitIndexes(value: number): IterableIterator<number> {
-  for (let index = 0; value > 0; index++, value >>= 1) {
-    if (value & 1) yield index;
+export function getHighBitCols(rows: Bit[][]): Bit[] {
+  return Array.from(rows[0], (_, index) => {
+    const col = rows.map((row) => row[index]);
+    return col.filter(Boolean).length > (rows.length / 2) ? 1 : 0;
+  });
+}
+
+export function invertBits(bits: Bit[]): Bit[] {
+  return bits.map((bit) => bit ? 0 : 1);
+}
+
+export function bitsToNumber(bits: Bit[]): number {
+  let value = 0;
+  for (const bit of bits) {
+    value = (value << 1) + bit;
   }
+  return value;
 }
 
-export function countBits(diagnostics: number[]): Map<number, number> {
-  const counts = new Map<number, number>();
-  for (const value of diagnostics) {
-    for (const index of getBitIndexes(value)) {
-      counts.set(index, (counts.get(index) ?? 0) + 1);
-    }
-  }
-  return counts;
-}
-
-export function getMajorityBits(
-  size: number,
-  counts: Map<number, number>,
-): { 0: number; 1: number } {
-  const result = { 0: 0, 1: 0 };
-  for (const [index, count] of counts) {
-    result[count > size / 2 ? 1 : 0] |= 1 << index;
-  }
-  return result;
-}
-
-export function getPowerConsumption(diagnostics: number[]): number {
-  const counts = countBits(diagnostics);
-  const {
-    0: gammaRate,
-    1: epsilonRate,
-  } = getMajorityBits(diagnostics.length, counts);
-  return gammaRate * epsilonRate;
+export function getRates(diagnostics: Bit[][]): {
+  gamma: number;
+  epsilon: number;
+} {
+  const bits = getHighBitCols(diagnostics);
+  return {
+    gamma: bitsToNumber(bits),
+    epsilon: bitsToNumber(invertBits(bits)),
+  };
 }
--- 2021/day/3/test.ts
+++ 2021/day/3/test.ts
@@ -1,9 +1,9 @@
 import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
 import {
-  countBits,
-  getBitIndexes,
-  getMajorityBits,
-  getPowerConsumption,
+  bitsToNumber,
+  getHighBitCols,
+  getRates,
+  invertBits,
   parseDiagnostics,
 } from "./mod.ts";
 
@@ -24,64 +24,61 @@ const exampleInput = `
 const input = await Deno.readTextFile(new URL("./input.txt", import.meta.url));
 
 Deno.test("parseDiagnostics", () => {
-  assertEquals(
-    parseDiagnostics(exampleInput),
-    [
-      0b00100,
-      0b11110,
-      0b10110,
-      0b10111,
-      0b10101,
-      0b01111,
-      0b00111,
-      0b11100,
-      0b10000,
-      0b11001,
-      0b00010,
-      0b01010,
-    ],
-  );
+  const diagnostics = parseDiagnostics(exampleInput);
+  assertEquals(diagnostics, [
+    [0, 0, 1, 0, 0],
+    [1, 1, 1, 1, 0],
+    [1, 0, 1, 1, 0],
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1],
+    [0, 1, 1, 1, 1],
+    [0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0],
+    [1, 0, 0, 0, 0],
+    [1, 1, 0, 0, 1],
+    [0, 0, 0, 1, 0],
+    [0, 1, 0, 1, 0],
+  ]);
 });
 
-Deno.test("getBitIndexes", () => {
-  assertEquals([...getBitIndexes(0b10110)], [1, 2, 4]);
-});
-
-Deno.test("countBits", () => {
+Deno.test("getHighBitCols", () => {
   assertEquals(
-    countBits([
-      0b00100,
-      0b11110,
-      0b10110,
-      0b10111,
-      0b10101,
-      0b01111,
-      0b00111,
-      0b11100,
-      0b10000,
-      0b11001,
-      0b00010,
-      0b01010,
+    getHighBitCols([
+      [0, 0, 1, 0, 0],
+      [1, 1, 1, 1, 0],
+      [1, 0, 1, 1, 0],
+      [1, 0, 1, 1, 1],
+      [1, 0, 1, 0, 1],
+      [0, 1, 1, 1, 1],
+      [0, 0, 1, 1, 1],
+      [1, 1, 1, 0, 0],
+      [1, 0, 0, 0, 0],
+      [1, 1, 0, 0, 1],
+      [0, 0, 0, 1, 0],
+      [0, 1, 0, 1, 0],
     ]),
-    new Map([[0, 5], [1, 7], [2, 8], [3, 5], [4, 7]]),
+    [1, 0, 1, 1, 0],
   );
 });
 
-Deno.test("getMajorityBits", () => {
-  assertEquals(
-    getMajorityBits(12, new Map([[0, 5], [1, 7], [2, 8], [3, 5], [4, 7]])),
-    { 0: 0b01001, 1: 0b10110 },
-  );
+Deno.test("invertBits", () => {
+  assertEquals(invertBits([1, 0, 1, 1, 0]), [0, 1, 0, 0, 1]);
+});
+
+Deno.test("bitsToNumber", () => {
+  assertEquals(bitsToNumber([1, 0, 1, 1, 0]), 0b10110);
 });
 
 Deno.test("part 1 example", () => {
   const diagnostics = parseDiagnostics(exampleInput);
-  const powerConsumption = getPowerConsumption(diagnostics);
-  assertEquals(powerConsumption, 198);
+  const rates = getRates(diagnostics);
+  assertEquals(rates.gamma, 0b10110);
+  assertEquals(rates.epsilon, 0b01001);
+  assertEquals(rates.gamma * rates.epsilon, 198);
 });
 
 Deno.test("part 1", () => {
   const diagnostics = parseDiagnostics(input);
-  const powerConsumption = getPowerConsumption(diagnostics);
-  assertEquals(powerConsumption, 4191876);
+  const rates = getRates(diagnostics);
+  assertEquals(rates.gamma * rates.epsilon, 4191876);
 });
