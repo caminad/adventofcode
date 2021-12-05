0caa46136a49d24149d83cb46540e738f8098680
David Jones <david@omg.lol>
2021-12-05 19:54:07 +0000

    sync std version

--- 2021/day/2/test.ts
+++ 2021/day/2/test.ts
@@ -1,7 +1,7 @@
 import {
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.116.0/testing/asserts.ts";
+} from "https://deno.land/std@0.117.0/testing/asserts.ts";
 import {
   Direction,
   move1,
