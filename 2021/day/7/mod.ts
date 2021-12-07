d305fc6a4a99a3c9f4bf363ba18e02588b023e72
David Jones <david@omg.lol>
2021-12-07 11:08:57 +0000

    day 7 part 2

--- 2021/day/7/mod.ts
+++ 2021/day/7/mod.ts
@@ -2,15 +2,32 @@ export function parseInput(input: string): number[] {
   return input.match(/\d+/g)!.map(Number);
 }
 
-export function getCost(positions: number[], destination: number): number {
-  return positions.reduce((sum, p) => sum + Math.abs(destination - p), 0);
+export function distance(origin: number, destination: number): number {
+  return Math.abs(origin - destination);
+}
+
+export function getCost(
+  values: number[],
+  costFn: (value: number) => number,
+): number {
+  return values.reduce((sum, value) => sum + costFn(value), 0);
+}
+
+export function triangle(value: number): number {
+  return value * (value + 1) / 2;
 }
 
 export function findCheapestDestination(
   positions: number[],
-): [destination: number, cost: number] {
-  const destinationCosts: [destination: number, cost: number][] = Array.from({
-    length: Math.max(...positions) + 1,
-  }, (_, destination) => [destination, getCost(positions, destination)]);
-  return destinationCosts.sort((a, b) => a[1] - b[1])[0];
+  costFn: (distance: number) => number = (d) => d,
+): { cost: number; destination: number } {
+  let cheapestDestination = { cost: Infinity, destination: -1 };
+  for (const destination of Array(Math.max(...positions) + 1).keys()) {
+    const distances = positions.map((p) => distance(p, destination));
+    const cost = distances.reduce((sum, d) => sum + costFn(d), 0);
+    if (cost < cheapestDestination.cost) {
+      cheapestDestination = { cost, destination };
+    }
+  }
+  return cheapestDestination;
 }
--- 2021/day/7/test.ts
+++ 2021/day/7/test.ts
@@ -1,5 +1,10 @@
 import { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
-import { findCheapestDestination, getCost, parseInput } from "./mod.ts";
+import {
+  distance,
+  findCheapestDestination,
+  parseInput,
+  triangle,
+} from "./mod.ts";
 
 const input = await Deno.readTextFile(new URL("input.txt", import.meta.url));
 
@@ -10,17 +15,53 @@ Deno.test("parseInput", () => {
   assertEquals(parseInput(exampleInput), exampleParsedInput);
 });
 
-Deno.test("getCost", () => {
-  assertEquals(getCost(exampleParsedInput, 1), 41);
-  assertEquals(getCost(exampleParsedInput, 2), 37);
-  assertEquals(getCost(exampleParsedInput, 3), 39);
-  assertEquals(getCost(exampleParsedInput, 10), 71);
+Deno.test("distance", () => {
+  assertEquals(
+    exampleParsedInput.map((p) => distance(p, 2)),
+    [14, 1, 0, 2, 2, 0, 5, 1, 0, 12],
+  );
+  assertEquals(
+    exampleParsedInput.reduce((a, e) => a + distance(e, 1), 0),
+    41,
+  );
+  assertEquals(
+    exampleParsedInput.reduce((a, e) => a + distance(e, 3), 0),
+    39,
+  );
+  assertEquals(
+    exampleParsedInput.reduce((a, e) => a + distance(e, 10), 0),
+    71,
+  );
 });
 
 Deno.test("findCheapestDestination", () => {
-  assertEquals(findCheapestDestination(exampleParsedInput), [2, 37]);
+  assertEquals(findCheapestDestination(exampleParsedInput), {
+    cost: 37,
+    destination: 2,
+  });
+});
+
+Deno.test("triangle", () => {
+  assertEquals(
+    Array.from(Array(10).keys(), triangle),
+    [0, 1, 3, 6, 10, 15, 21, 28, 36, 45],
+  );
+});
+
+Deno.test("expensive movement", () => {
+  assertEquals(findCheapestDestination(exampleParsedInput, triangle), {
+    cost: 168,
+    destination: 5,
+  });
 });
 
 Deno.test("part 1", () => {
-  assertEquals(findCheapestDestination(parseInput(input))[1], 335330);
+  assertEquals(findCheapestDestination(parseInput(input)).cost, 335330);
+});
+
+Deno.test("part 2", () => {
+  assertEquals(
+    findCheapestDestination(parseInput(input), triangle).cost,
+    92439766,
+  );
 });
