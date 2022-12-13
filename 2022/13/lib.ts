ec24b3603fec8c5e29a706404eb0b9e9fbe20760
caminad <418@omg.lol>
2022-12-13 20:14:06 +0000

    2022/13 part 2

--- 2022/13/lib.ts
+++ 2022/13/lib.ts
@@ -1,16 +1,22 @@
-export type Packet = ReadonlyArray<PacketValue>;
 export type PacketValue = Packet | number;
+export type Packet = ReadonlyArray<PacketValue>;
 export function Packet(value: PacketValue): Packet {
   return Array.isArray(value) ? value : [value];
 }
 
-export function* parse(input: string): IterableIterator<[Packet, Packet]> {
-  let prev: string | undefined;
-  for (const [curr] of input.matchAll(/[\[\d,\]]+/g)) {
+export function* parse(input: string): IterableIterator<Packet> {
+  for (const [text] of input.matchAll(/\[[\[\d,\]]+/g)) {
+    yield JSON.parse(text);
+  }
+}
+
+export function* pairs<T>(iter: Iterable<T>): IterableIterator<[T, T]> {
+  let prev: T | undefined;
+  for (const curr of iter) {
     if (prev === undefined) {
       prev = curr;
     } else {
-      yield [JSON.parse(prev), JSON.parse(curr)];
+      yield [prev, curr];
       prev = undefined;
     }
   }
@@ -19,9 +25,9 @@ export function* parse(input: string): IterableIterator<[Packet, Packet]> {
 function* zipLongest<T, U>(
   a: readonly T[],
   b: readonly U[],
-): IterableIterator<[T, U]> {
+): IterableIterator<[T | undefined, U | undefined]> {
   for (let i = 0; i < Math.max(a.length, b.length); i++) {
-    yield [a[i], b[i]];
+    yield [a.at(i), b.at(i)];
   }
 }
 
@@ -30,11 +36,11 @@ export function cmp(a: PacketValue, b: PacketValue): number {
     return Math.sign(a - b);
   }
   for (const [p, q] of zipLongest(Packet(a), Packet(b))) {
-    if (q === undefined) {
-      return 1;
-    }
     if (p === undefined) {
       return -1;
+    }
+    if (q === undefined) {
+      return 1;
     }
     const c = cmp(p, q);
     if (c !== 0) {
@@ -46,4 +52,10 @@ export function cmp(a: PacketValue, b: PacketValue): number {
 
 export function ordered(a: Packet, b: Packet): boolean {
   return cmp(a, b) <= 0;
+}
+
+export function decoderKey(packets: Iterable<Packet>): number {
+  const dividers = [[[2]], [[6]]] as const;
+  const sorted = [...dividers, ...packets].sort(cmp);
+  return (sorted.indexOf(dividers[0]) + 1) * (sorted.indexOf(dividers[1]) + 1);
 }
